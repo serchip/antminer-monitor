7fdb2e09b59b8fa4fca1a89e18f70c682c66e2b3
ser chip <s.chip@crmcard.ru>
2018-06-19 22:44:39 +0300

    utf-8

--- create_db.py
+++ create_db.py
@@ -1,3 +1,4 @@
+#-*- coding:utf-8 -*-
 from app.models import MinerModel, Settings, MinerСontainer
 from app import db
 from sqlalchemy.exc import IntegrityError
--- update_db.py
+++ update_db.py
@@ -1,3 +1,4 @@
+#-*- coding:utf-8 -*-
 from app.models import Miner, MinerModel, Settings, MinerСontainer
 from app import db
 
