05d26beff74fc06e63be6c42219fccc049b1d475
ser chip <s.chip@crmcard.ru>
2018-06-21 17:17:03 +0300

    fix

--- app/views/antminer_json.py
+++ app/views/antminer_json.py
@@ -1,24 +1,24 @@
 from flask import jsonify
-from app import app
+from app import application
 from lib.pycgminer import (get_summary,
                                get_pools,
                                get_stats,
                                )
 
 
-@app.route('/<ip>/summary')
+@application.route('/<ip>/summary')
 def summary(ip):
     output = get_summary(ip)
     return jsonify(output)
 
 
-@app.route('/<ip>/pools')
+@application.route('/<ip>/pools')
 def pools(ip):
     output = get_pools(ip)
     return jsonify(output)
 
 
-@app.route('/<ip>/stats')
+@application.route('/<ip>/stats')
 def stats(ip):
     output = get_stats(ip)
     return jsonify(output)
