d70ad1941c583f2d89447efd95d00cd8619895a7
ser chip <s.chip@crmcard.ru>
2018-06-25 15:51:16 +0300

    fix

--- app/views/antminer.py
+++ app/views/antminer.py
@@ -54,7 +54,7 @@ def miners():
     for miner in miners:
         miner_stats = miner.get_stats()
         # if miner not accessible
-        if miner_stats.get('STATUS', 'error')[0]['STATUS'] == 'error':
+        if not miner_stats or miner_stats.get('STATUS') and miner_stats['STATUS'][0]['STATUS'] == 'error':
             errors = True
             inactive_miners.append(miner)
         else:
